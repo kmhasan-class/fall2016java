04b6edd8efb360e231ab54046af576ffe60d9964
Monirul Hasan <kmhasan@gmail.com>
2016-10-03 11:36:57 +0600

    lecture 1

---
+++ lecture1/Hello World/src/hello/world/HelloWorld.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world;
+
+/**
+ *
+ * @author kmhasan
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+        int data[] = {3, 5, 17, 3, 2};
+        
+        for (i = 1; i <= 5; i++)
+            System.out.printf("%d\n", i);
+    }
+    
+}
---
+++ lecture1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	public static void main(String args[]) {
+		int i;
+		for (i = 1; i <= 5; i++)
+			System.out.printf("%d\n", i);
+	}
+}
+
