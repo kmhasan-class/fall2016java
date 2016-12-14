a64b2ecbef120dab27958fb2652dc23728f28b31
Monirul Hasan <kmhasan@gmail.com>
2016-12-14 10:19:09 +0600

    Chat server and client

--- Chat Application/src/chat/application/ChatServer.java
+++ Chat Application/src/chat/application/ChatServer.java
@@ -7,8 +7,12 @@ package chat.application;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,6 +27,8 @@ public class ChatServer {
             ServerSocket serverSocket = new ServerSocket(9999);
             System.out.println("Started the chat server at port 9999");
 
+            
+                        
             while (true) {
                 System.out.println("Waiting on clients ...");
                 Socket clientSocket = serverSocket.accept();
@@ -33,6 +39,11 @@ public class ChatServer {
                 inputStream.read(messageBytes);
                 String messageString = new String(messageBytes);
                 System.out.println(messageString);
+                
+                OutputStream outputStream = clientSocket.getOutputStream();
+                String dateTime = LocalDate.now() + " " + LocalTime.now();
+                messageString = dateTime + " " + messageString;
+                outputStream.write(messageString.getBytes());
             }
 
         } catch (IOException ex) {
